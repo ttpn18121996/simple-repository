973ef91bdb582da28ea619114913c1387e557158
ttpn18121996 <ttpn18121996@gmail.com>
2023-10-27 17:52:02 +0700

    Update Service and README

--- src/Service.php
+++ src/Service.php
@@ -2,20 +2,52 @@
 
 namespace SimpleRepository;
 
+use Exception;
 use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 abstract class Service
 {
-    public function __get($property)
+    protected array $authUsers;
+
+    /**
+     * Get the authenticated user for the service.
+     */
+    public function authUser($guard = null)
     {
-        $className = Str::of($property)->studly();
+        $guard ??= Config::get('auth.defaults.guard');
+
+        return Arr::get($this->authUsers, $guard);
+    }
+
+    /**
+     * Set the authenticated user for the service.
+     */
+    public function useAuthUser($user, $guard = null)
+    {
+        $guard ??= Config::get('auth.defaults.guard');
+
+        $this->authUsers[$guard] = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get a new service instance.
+     *
+     * @throws \Exception
+     */
+    public function getService(string $serviceName)
+    {
+        $className = Str::of($serviceName)->studly();
         $service = '\\App\\Services\\'.$className->toString();
 
-        if ($className->endsWith('Service') && class_exists($service)) {
+        if (class_exists($service)) {
             return Container::getInstance()->make($service);
         }
 
-        return $this?->{$property} ?? null;
+        throw new Exception($service.' does not exist.');
     }
 }
