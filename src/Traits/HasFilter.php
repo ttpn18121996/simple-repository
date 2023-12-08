08710ce959dd21068e1f78f7cae3393259e76b86
ttpn18121996 <ttpn18121996@gmail.com>
2023-12-08 09:28:20 +0700

    Supports orSearch and orFilter

--- src/Traits/HasFilter.php
+++ src/Traits/HasFilter.php
@@ -17,7 +17,9 @@ trait HasFilter
     protected function buildFilter(Builder $query, array $filters = []): Builder
     {
         $search = Arr::get($filters, 'search');
+        $orSearch = Arr::get($filters, 'or_search');
         $filter = Arr::get($filters, 'filter');
+        $orFilter = Arr::get($filters, 'or_filter');
         $sort = Arr::get($filters, 'sort');
 
         if (! empty($search)) {
@@ -28,10 +30,26 @@ trait HasFilter
             });
         }
 
+        if (! empty($orSearch)) {
+            $query->where(function ($query) use ($orSearch) {
+                foreach ($orSearch as $field => $value) {
+                    $query->orWhere($field, 'like', "%{$value}%");
+                }
+            });
+        }
+
         if (! empty($filter)) {
             $query->where(function ($query) use ($filter) {
                 foreach ($filter as $field => $value) {
                     $query->where($field, $value);
+                }
+            });
+        }
+        
+        if (! empty($orFilter)) {
+            $query->where(function ($query) use ($orFilter) {
+                foreach ($orFilter as $field => $value) {
+                    $query->orWhere($field, $value);
                 }
             });
         }
